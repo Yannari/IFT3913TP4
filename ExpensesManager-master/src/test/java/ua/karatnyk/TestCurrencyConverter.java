ccda991f2d6b171df37f347e32a43618059d2171
Yannari <33236948+Yannari@users.noreply.github.com>
2022-12-14 15:30:41 -0500

    Add files via upload

---
+++ ExpensesManager-master/src/test/java/ua/karatnyk/TestCurrencyConverter.java
@@ -0,0 +1,17 @@
+package ua.karatnyk;
+
+import org.junit.jupiter.api.Test;
+import ua.karatnyk.impl.CurrencyConversion;
+import ua.karatnyk.impl.CurrencyConvertor;
+import ua.karatnyk.impl.OfflineJsonWorker;
+
+import java.text.ParseException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestCurrencyConvertor {
+ca
+    
+}
