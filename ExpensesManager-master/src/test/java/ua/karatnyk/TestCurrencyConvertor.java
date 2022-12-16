017acc0426776568939ba058dc867c8e35a7dace
Yannari <33236948+Yannari@users.noreply.github.com>
2022-12-16 18:16:19 -0500

    Update

--- ExpensesManager-master/src/test/java/ua/karatnyk/TestCurrencyConvertor.java
+++ ExpensesManager-master/src/test/java/ua/karatnyk/TestCurrencyConvertor.java
@@ -149,9 +149,33 @@ public class TestCurrencyConvertor {
 			fail("currency not currently support");
 		}
 	}
-	
 	@Test 
 	public void currency16() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(0, "CAD" , "CAD", conversion);
+			assertTrue(output == 0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
+	@Test 
+	public void currency17() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(0, "CAD" , "KOR", conversion);
+			assertTrue(output == 0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
+	
+	@Test 
+	public void currency18() throws ParseException{
 		try{
 			conversion  = new OfflineJsonWorker().parser();
 			double output = CurrencyConvertor.convert(10000, "CAD" , "USD", conversion);
@@ -162,9 +186,31 @@ public class TestCurrencyConvertor {
 			fail("currency not currently support");
 		}
 	}
+	public void currency19() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(10000, "CAD" , "CAD", conversion);
+			assertTrue(output>0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
+	public void currency20() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(10000, "CAD" , "KOR", conversion);
+			assertTrue(output>0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
 	
 	@Test (expected = ParseException.class)
-	public void currency17() throws ParseException{
+	public void currency21() throws ParseException{
 		try{
 
 		
@@ -178,14 +224,71 @@ public class TestCurrencyConvertor {
 			fail("currency not currently support");
 		}
 		
-	}
+	} 
+	public void currency22() throws ParseException{
+		try{
+
+		
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(-1, "CAD" , "CAD", conversion);
+			
+			assertFalse(output>0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+		
+	} 
+	public void currency23() throws ParseException{
+		try{
+
+		
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(-1, "CAD" , "KOR", conversion);
+			
+			assertFalse(output>0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+		
+	} 
 	
 	@Test (expected = ParseException.class)
-	public void currency18() throws ParseException{
+	public void currency24() throws ParseException{
 		try{
 			conversion  = new OfflineJsonWorker().parser();
 			double output = CurrencyConvertor.convert(10001, "CAD" , "USD", conversion);
 			assertFalse(output<0);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+		
+	}
+	@Test (expected = ParseException.class)
+	public void currency25() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(10001, "CAD" , "KOR", conversion);
+			
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+		
+	}
+	@Test (expected = ParseException.class)
+	public void currency26() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(10001, "CAD" , "CAD", conversion);
+			assertTrue(output==10001);
+			
 		}
 		catch(Exception e)
 		{
@@ -199,7 +302,7 @@ public class TestCurrencyConvertor {
 	 */
 	
 	@Test 
-	public void currency19() throws ParseException{
+	public void currency27() throws ParseException{
 		try{
 			conversion  = new OfflineJsonWorker().parser();
 			double output = CurrencyConvertor.convert(1, "CAD" , "USD", conversion);
@@ -211,13 +314,58 @@ public class TestCurrencyConvertor {
 			fail("currency not currently support");
 		}
 	}
+	public void currency28() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(1, "CAD" , "CAD", conversion);
+			
+			assertTrue(output == 1);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
+	public void currency29() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(1, "CAD" , "KOR", conversion);
+			
+			
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
 	
 	@Test 
-	public void currency20() throws ParseException{
+	public void currency30() throws ParseException{
 		try{
 			conversion  = new OfflineJsonWorker().parser();
 			double output = CurrencyConvertor.convert(9999, "CAD" , "USD", conversion);
 			assertTrue(output == 7437.791688936396);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
+	public void currency31() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(9999, "CAD" , "CAD", conversion);
+			assertTrue(output == 9999);
+		}
+		catch(Exception e)
+		{
+			fail("currency not currently support");
+		}
+	}
+	public void currency32() throws ParseException{
+		try{
+			conversion  = new OfflineJsonWorker().parser();
+			double output = CurrencyConvertor.convert(9999, "CAD" , "KOR", conversion);
 		}
 		catch(Exception e)
 		{
